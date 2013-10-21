927a86408291041f87f9e9a4e13d4a334ecfd5ab
Dan Worth <djworth@dramafever.com>
2013-10-21 19:46:32 -0400

    A crude impl of realtime chat with usernames yay!  Inspired/copied from http://gary.beagledreams.com/page/go-websocket-chat.html

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func main() {
+	flag.Parse()
+	go h.run()
+
+	http.Handle("/ws", websocket.Handler(wsHandler))
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
