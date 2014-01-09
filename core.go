c30057a4a4a3a4acad9cdbc95fe3ce68a61f4ece
James Polera <james@uncryptic.com>
2014-01-09 00:07:03 -0500

    More basic login hacking - currently, any login works

--- core.go
+++ core.go
@@ -1,47 +1,40 @@
 package main
 
 import (
-	"bufio"
+	_ "bufio"
 	"encoding/xml"
 	"fmt"
+	_ "io"
 	"net"
-_    "io"
 )
 
-type Stream struct {
-    XMLName xml.Name `xml:"stream:stream"`
-    Iq
-}
-
 type Query struct {
-    XMLName xml.Name `xml:"query"`
-    Xmlns string `xml:"xmlns,attr"`
-    Username string `xml:"username"`
-    Password string `xml:"password"`
-    Digest string `xml:"digest"`
-    Resource string `xml:"resource"`
+	XMLName  xml.Name `xml:"query"`
+	Xmlns    string   `xml:"xmlns,attr"`
+	Username string   `xml:"username"`
+	Password string   `xml:"password"`
+	Digest   string   `xml:"digest"`
+	Resource string   `xml:"resource"`
 }
-
 
 type Iq struct {
-    XMLName xml.Name `xml:"iq"`
-	Type string `xml:"type,attr"`
-	Id   string `xml:"id,attr"`
-    Query
+	XMLName xml.Name `xml:"iq"`
+	Type    string   `xml:"type,attr"`
+	Id      string   `xml:"id,attr"`
+	Query   Query    `xml:"query"`
 }
 
+type Stream struct {
+	XMLName xml.Name `xml:"stream:stream"`
+	Iq      Iq       `xml:"iq"`
+}
 
 type LoginReq struct {
-	//<?xml version='1.0' ?><stream:stream to='localhost' xmlns='jabber:client'
-	//xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>
-	//<iq type='get' id='purple3d9c697f'><query xmlns='jabber:iq:auth'>
-	//<username>james</username></query></iq></stream:stream>
 	Iq Iq `xml:"iq"`
 }
 
-
 func Serve(addr string) {
-    fmt.Println("Server started")
+	fmt.Println("Server started")
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		// handle error
@@ -56,25 +49,28 @@ func Serve(addr string) {
 	}
 }
 
-func handleLogin(loginRequest string) (x string, e error) {
+func ParseIq(request string) (iq Iq, e error) {
 
 	v := LoginReq{}
 
-	err := xml.Unmarshal([]byte(loginRequest), &v)
+	err := xml.Unmarshal([]byte(request), &v)
 
 	if err != nil {
 		fmt.Printf("err: %v", err)
-		return "", err
+		return v.Iq, err
 	}
 	fmt.Printf("Id: %v\n", v.Iq.Id)
-    return v.Iq.Id, err
+	return v.Iq, err
+}
+
+func Authenticate(username string, password string) (authXml string) {
+	return "hey"
 }
 
 func handleConnection(conn net.Conn) {
 	fmt.Printf("Handling connection....")
 
-	b := bufio.NewReader(conn)
-
+    b := xml.NewDecoder(conn)
 	fmt.Fprintf(conn, `<?xml version='1.0'?>
 		<stream:stream
 			from='localhost'
@@ -87,28 +83,25 @@ func handleConnection(conn net.Conn) {
 
 	fmt.Fprintf(conn, "<stream:features/>")
 	for {
-		line, _, err := b.ReadLine()
-		if err != nil {
-			break
+        iqData := new(Iq)
+        b.Decode(iqData)
+		
+		switch iqData.Type {
+		case "get":
+            r := &Iq{Id: iqData.Id, Type: "result"}
+			r.Query = Query{Xmlns: "jabber:iq:auth"}
+			output, _ := xml.Marshal(r)
+			fmt.Println(string(output))
+			fmt.Fprintf(conn, string(output))
+		case "set":
+			fmt.Println("** Case Set **")
+            i := Iq{Id: iqData.Id, Type: "result"}
+			output2, _ := xml.Marshal(i)
+			fmt.Println(string(output2))
+			fmt.Fprintf(conn, string(output2))
+		default:
+			//fmt.Println(string(line))
 		}
-		loginId, err := handleLogin(string(line))
-        fmt.Println(loginId)
-		fmt.Println(string(line))
-        r := &Stream{}
-        r.Iq = Iq{Id: loginId, Type: "result"}
-        r.Iq.Query = Query{Xmlns: "jabber:iq:auth"}
-        output, err := xml.Marshal(r)
-        fmt.Println(string(output))
-        
-        
-        line, _, err = b.ReadLine()
-        loginId2, err := handleLogin(string(line))
-        i := &Stream{}
-        i.Iq = Iq{Id: loginId2, Type: "result"}
-        output2, err := xml.Marshal(i)
-        fmt.Println(string(output2))
-        //fmt.Fprintf(conn, string(output))
 	}
-
 
 }
